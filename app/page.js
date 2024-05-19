e12da24ccc401a08a2fd3a10b8601b44b1eababc
m-shao <s201083816@ddsbstudent.ca>
2024-05-19 00:49:57 -0400

    home page, road map page, intro page

--- app/page.js
+++ app/page.js
@@ -4,12 +4,29 @@ import { useEffect, useState } from 'react';
 import Image from 'next/image';
 
 import Home from '@/components/pages/Home.jsx';
+import RoadMap from '@/components/pages/RoadMap.jsx';
+import Introduction from '@/components/pages/Introduction.jsx';
 
 const Page = () => {
 	const [page, setPage] = useState(0);
-	const pages = [<Home key='0' />];
+	const pages = [
+		<Home key='0' setPage={setPage} />,
+		<Introduction key='1' setPage={setPage} />,
+	];
 
-	return <div>{pages[0]}</div>;
+	return (
+		<div>
+			{page % 2 === 0 ? (
+				pages[page / 2]
+			) : (
+				<RoadMap
+					key='1'
+					setPage={setPage}
+					index={Math.floor(page / 2)}
+				/>
+			)}
+		</div>
+	);
 };
 
 export default Page;
